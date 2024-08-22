38f840d9ba0320982980c7d1bf27addcd1dd5e20
Andrew <win1887@yandex.ru>
2024-08-22 15:12:32 +0300

    fixed recipe inlines

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -37,6 +37,11 @@ class IngredientsInline(admin.TabularInline):
     model = RecipeIngredient
     extra = 0
     min_num = 1
+    
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj=None, **kwargs)
+        formset.validate_min = True
+        return formset
 
 
 @admin.register(Recipe)
